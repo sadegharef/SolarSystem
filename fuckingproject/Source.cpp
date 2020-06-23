ee38bfbc57b8fe762ea8bd044b0b93a742cba3ad
sadegharef <sadegh_ghalati@yahoo.com>
2020-06-23 14:25:09 +0430

    v1

--- fuckingproject/Source.cpp
+++ fuckingproject/Source.cpp
@@ -8,14 +8,14 @@ using namespace std;
 class Point
 {
 public:
-    float x, y, z;
-    void setxyz(float x2, float y2, float z2)
+    GLfloat x, y, z;
+    void setxyz(GLfloat x2, GLfloat y2, GLfloat z2)
     {
         x = x2;
         y = y2;
         z = z2;
     }
-    void setxy(float x2, float y2)
+    void setxy(GLfloat x2, GLfloat y2)
     {
         x = x2;
         y = y2;
@@ -33,10 +33,8 @@ public:
 
 //define global variable
 Point abc[4];
-Point G[51];
-int SCREEN_HEIGHT = 500;
+Point G[52];
 int keyframe = 4;
-int points = 0;
 //define cube point and cube angle
 
 //factorial
@@ -69,29 +67,36 @@ Point drawBezierGeneralized(Point PT[], float t) {
     }
     return P;
 }
-//estimating the angle between a normalize point and cube point
-float estimateAngle(Point PT[], Point cube, int i) {
-    float adotb = (PT[i].x * cube.x) + (PT[i].y * cube.y) + (PT[i].z * cube.z);
-    float a = sqrt(pow(PT[i].x, 2) + pow(PT[i].y, 2) + pow(PT[i].z, 2));
-    float b = sqrt(pow(cube.x, 2) + pow(cube.y, 2) + pow(cube.z, 2));
-
-    return acosf(adotb / (a * b));
-}
 //define the init
 void myInit() {
     glClearColor(.0, .0, .0, 0.0);
     glColor3f(0.0, 0.0, 0.0);
-    glPointSize(3);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0.0, 640.0, 0.0, 500.0);
+    abc[0].setxyz(0.0, 0.0, 3.0);
+    abc[1].setxyz(2.0, -2.5, 2.0);
+    abc[2].setxyz(0.0, 3.0, -3.0);
+    abc[3].setxyz(3.0, 0.0, 0.0);
+   
+    int i = 1;
+    G[0] = abc[0];
+    for (float t = 0.0f; t <= 1.0f; t += 0.02f)
+    {
+        Point p2 = drawBezierGeneralized(abc, t);
+        cout << p2.x << ",  " << p2.y << ", " << p2.z << endl;
+        cout << endl;
+        G[i] = p2;
+        i++;
+    }
 }
 //drawing function
+int points = 0;
 void draw() {
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     //first cube
     glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 3.0f);
+    glTranslatef(G[points].x, G[points].y, G[points].z);
     glColor3f(1, 1, 1);
     glScalef(0.8f, 1.6f, 0.4f);
     glutSolidCube(.4);
@@ -99,28 +104,39 @@ void draw() {
     //flush
     glFlush();
 }
+//define constance speed
+void timer(int v) {
+    // Updata movement parameters
+    if (points == 51)
+        return;
+    points++;
+
+    glutPostRedisplay();
+    glutTimerFunc(100, timer, 0);
+}
 //on mouse click function
 void myMouse(int button, int state, int x, int y) {
     // If left button was clicked
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        abc[0].setxyz(-0.6, 0.0, 0.0);
-        abc[1].setxyz(0.0, 10.0, 0.0);
-        abc[2].setxyz(0.4, 10.0, 20.0);
-        abc[3].setxyz(0.4, 0.0, 0.0);
+        glutTimerFunc(100, timer, 0);
+    }
+}
+//define the variable speed
+void keyboard(unsigned char key, int x, int y)
+{
 
+    switch (key) {
+    case '1':
+        points++;
+        glutPostRedisplay();
+        break;
 
-        glColor3f(0.2, 1.0, 0.0);
-        int i = 0;
-        Point p1 = abc[0];
-        for (float t = 0.0; t <= 1.0; t += 0.02f)
-        {
-            Point p2 = drawBezierGeneralized(abc, t);
-            cout << p2.x << ",  " << p2.y << ", " << p2.z << endl;
-            cout << endl;
-            G[i] = p2;
-            i++;
-        }
-        glColor3f(0.0, 0.0, 0.0);
+    case '2':
+        points--;
+        glutPostRedisplay();
+        break;
+    default:
+        break;
     }
 }
 //reshaping
@@ -130,46 +146,11 @@ void reshape(int w, int h) {
         gluPerspective(atan(tan(50.0 * 3.14159 / 360.0) / 1.0) * 360.0 / 3.141593, 1.0, 3.0, 7.0);
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        gluLookAt(5.0, 3.0, 5.0,
+        gluLookAt(5.0, 2.0, 5.0,
             0.0, 0.0, 0.0,
             0.0, 1.0, 0.0);
 }
-//define the variable speed
-void keyboard(unsigned char key, int x, int y)
-{
 
-    switch (key) {
-    case '1':
-        glutPostRedisplay();
-        break;
-
-    case '2':
-        glutPostRedisplay();
-        break;
-
-    case '3':
-
-        glutPostRedisplay();
-        break;
-
-    case '4':
-
-        glutPostRedisplay();
-        break;
-
-    case '5':
-
-        glutPostRedisplay();
-        break;
-    case '6':
-
-        glutPostRedisplay();
-        break;
-
-    default:
-        break;
-    }
-}
 //main
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
@@ -179,10 +160,11 @@ int main(int argc, char* argv[]) {
     glutCreateWindow("Bezier Curve");
 
     myInit();
+    glutKeyboardFunc(keyboard);
     glutMouseFunc(myMouse);
     glutDisplayFunc(draw);
     glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
+    
     glutMainLoop();
 
     return 0;
