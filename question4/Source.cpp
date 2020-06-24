bb8bc1ddd7c9194345f40d8fb4f198eee3362f3b
sadegharef <sadegh_ghalati@yahoo.com>
2020-06-24 13:39:16 +0430

    commenting

--- question4/Source.cpp
+++ question4/Source.cpp
@@ -17,7 +17,7 @@ angleSun = 0.0,
 anglePlanet2 = 0.0,
 anglePlanet2_sun = 0.0;
 
-//define the colors
+//define the colors and gradient color for depth
 GLfloat black[] = { 0.0f, 0.0f, 0.0f, 0.1f };
 GLfloat white[] = { 0.0f, 1.0f, 1.0f, 1.0f };
 GLfloat blue[] = { 0.0f, 0.0f, 0.9f, 1.0f };
@@ -28,7 +28,7 @@ GLfloat qDif[] = { 1.0f, 1.0f, 1.0f, 0.5f };
 GLfloat qSpec[] = { .50f, .50f, .50f, 0.1f };
 GLfloat qPos[] = { 0.0f, 0.0f, 0.0f, 8.0f };
 
-//define the belt orbit of planet
+//define the belt orbit of planet **fail**
 //GLfloat sc[8] = { 0.295 , 0.40,0.50, 0.60,0.80,1.0,1.05,1.13 };
 //double ang = 2 * PI / 300;//for normal planet
 //double angular = 2 * PI / 50;// the belt of uranus
@@ -39,7 +39,7 @@ void initLighting()
     
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT7);
-
+    //eanble lighting for depth and showing the light of sun
     glLightfv(GL_LIGHT7, GL_AMBIENT, qAmb);
     glLightfv(GL_LIGHT7, GL_DIFFUSE, qDif);
     glLightfv(GL_LIGHT7, GL_SPECULAR, qSpec);
@@ -47,66 +47,59 @@ void initLighting()
 }
 void myinit()
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0); //backgroundcolor is green
-   
-    glPointSize(1.0);
-    glLineWidth(2.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0); //backgroundcolor black
+
+   //init the point size and line size for belt **fail**
+   // glPointSize(1.0);
+   // glLineWidth(2.0);
 
 }
 
-void background()
-{
-    glBegin(GL_POINT);
-    glColor3f(0.0, 0.00, 0.00);
-    glVertex3f(-01.00, 01.00, 1);
-    glEnd();
-}
 
 void draw(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    background();
     
-    glLoadIdentity();
-    glScalef(0.05, 0.05, 0.05);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_COLOR_MATERIAL);
+    glLoadIdentity();//load objects
+    glScalef(0.05, 0.05, 0.05);// scale the matrix that can be seen on screen
+    glEnable(GL_DEPTH_TEST);//enable depth
+    glEnable(GL_COLOR_MATERIAL);//enable color
     
     
 
     //making sun
     glPushMatrix();
-    glColor3f(6.0, 3.0, 0.0);
-    glRotatef(angleSun, 0.0, 1.0, 0.0);
-    glutSolidSphere(5, 20, 25);
+    glColor3f(6.0, 3.0, 0.0);//color
+    glRotatef(angleSun, 0.0, 1.0, 0.0);//rotation around itself
+    glutSolidSphere(5, 20, 25);//make with r=5
     glPopMatrix();
     //making planet2
     glPushMatrix();
-    glColor3f(3.0, 3.0, 8.0);
-    glRotatef(anglePlanet2_sun, 0.0, 1.0, 0.0);
-    glTranslatef(10 * cos(anglePlanet2_sun), 0, 8 * sin(anglePlanet2_sun));
-    glRotatef(anglePlanet2, 0.0, 1.0, 0.0);
-    glutSolidSphere(2, 20, 20);
+    glColor3f(3.0, 3.0, 8.0);//color
+    glRotatef(anglePlanet2_sun, 0.0, 1.0, 0.0);//rotation around the sun
+    glTranslatef(10 * cos(anglePlanet2_sun), 0, 8 * sin(anglePlanet2_sun));// make the rotation path an Ellipse
+    glRotatef(anglePlanet2, 0.0, 1.0, 0.0);//rotaton around itself
+    glutSolidSphere(2, 20, 20);// make sphere with r=2
     glPopMatrix();
     //make earth
     glPushMatrix();
-    glColor3f(.0, 0.1, 0.7);
-    glRotatef(angleEarth_sun, 0.0, 1.0, 0.0);
-    glTranslatef(15, 0.0, 0.0);
-    glRotatef(angleEarth_itself, 0.0, 1.0, 0.0);
-    glutSolidSphere(3, 20, 30);
+    glColor3f(.0, 0.1, 0.7);//color
+    glRotatef(angleEarth_sun, 0.0, 1.0, 0.0);//rotation around the sun
+    glTranslatef(15, 0.0, 0.0);// the circle path
+    glRotatef(angleEarth_itself, 0.0, 1.0, 0.0);//rotation around itself
+    glutSolidSphere(3, 20, 30);//make with r=3
     glPopMatrix();
     //make moon
     glPushMatrix();
-    glColor3f(3.0, 3.0, 8.0);
+    glColor3f(3.0, 3.0, 8.0);//color
     glRotatef(angleMoon_sun, 0.0, 1.0, 0.0);//rotation around sun
-    glTranslatef(15, 0, 0);
-    glRotatef(angleMoon_earth, 1.0, 0.0, 0.0);//around the earth
-    glTranslatef(0.0, 5.0, 0.0);
+    glTranslatef(15, 0, 0);// distance from sun
+    glRotatef(angleMoon_earth, 1.0, 0.0, -0.5);//around the earth
+    glTranslatef(0.0, 5.0, 0.0);//distance from earth
     glRotatef(angleSun, 0.0, 1.0, 0.0);//rotate around itself
     glutSolidSphere(0.5, 20, 30);
     glPopMatrix();
-    //lighting
+    //lighting for light of the sun and showing the depth
     glLightfv(GL_LIGHT7, GL_POSITION, qPos);
     glMaterialfv(GL_FRONT_AND_BACK, GL_COLOR_INDEXES, yellow);
     glMaterialfv(GL_FRONT_AND_BACK, GL_COLOR_INDEXES, black);
@@ -117,7 +110,7 @@ void draw(void)
 
 
 void timer(int value) {
-
+    //changing an angle to set the speed of rotation
     angleSun += 1;
     angleEarth_sun += 1;
     angleEarth_itself += 3;
@@ -133,13 +126,13 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(0, 0);
-    glutInitWindowSize(1080, 1080);
-    glutCreateWindow("Solar System");
-    initLighting();
+    glutInitWindowPosition(0, 0);//set window position
+    glutInitWindowSize(1080, 1080);//set window size
+    glutCreateWindow("Solar System");//creating a window
+    initLighting();// init light
     myinit();
-    glutDisplayFunc(draw);
-    glutTimerFunc(30, timer, 0);
+    glutDisplayFunc(draw);//draw the objects
+    glutTimerFunc(30, timer, 0);// timer
     glutMainLoop();
     return 0;
 }
